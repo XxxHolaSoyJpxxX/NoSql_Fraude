b9e755091a1153bc450852f77ba6542123bb9035
XxxHolaSoyJpxxX <juanpablo.conquistador@gmail.com>
2025-05-15 21:14:13 -0600

    Pendejada del menu de admin

--- core/admin.py
+++ core/admin.py
@@ -12,19 +12,19 @@ def menu_admin():
         print("\n=== Panel de Administración ===")
         print("1. Dashboard de monitoreo en tiempo real") #Todas las transacciones de todos los usuarios
         print("2. Ver todos los fraudes") 
-        print("5. Reportes y estadísticas de fraude") #estadisticas de tipo de fraude
-        print("6. Gestión de perfiles de riesgo") #top global de perfiles de fraude
-        print("7. Listas de vigilancia") #ver todos los perfiles con fraude
-        print("9. Auditoría de acciones de administradores") #mostrar toda la info de un usuario en especifico
-        #print("10. Configuración de umbrales de alerta")
-        print("12. Bloquear cuenta") #bloquear transacciones de un usuario
-        print("1. Desbloquear cuenta")
-        print("13. Aprobación de transacciones de alto riesgo") #cambiar el status de una transaccion fraude
-        print("16. Visualización de anomalías geográficas") #ver solamente los fraudes geograficos
-        print("19. Análisis de comportamiento de administradores") #ver todas las acciones de cuentas de administradores
-        print("1. Soporte")
-        print("1. Notificar usuario")
-        print("21. Salir")
+        print("3. Reportes y estadísticas de fraude") #estadisticas de tipo de fraude
+        print("4. Gestión de perfiles de riesgo") #top global de perfiles de fraude
+        print("5. Listas de vigilancia") #ver todos los perfiles con fraude
+        print("6. Auditoría de acciones de administradores") #mostrar toda la info de un usuario en especifico
+        #print("6.5. Configuración de umbrales de alerta")
+        print("7. Bloquear cuenta") #bloquear transacciones de un usuario
+        print("8. Desbloquear cuenta")
+        print("9. Aprobación de transacciones de alto riesgo") #cambiar el status de una transaccion fraude
+        print("10. Visualización de anomalías geográficas") #ver solamente los fraudes geograficos
+        print("11. Análisis de comportamiento de administradores") #ver todas las acciones de cuentas de administradores
+        print("12. Soporte")
+        print("13. Notificar usuario")
+        print("14. Salir")
 
         opcion = input("Selecciona una opción: ")
 
